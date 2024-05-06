871ea73b85949d3eff52b17d845159424343022b
fim4re <fimareyassine@gmail.com>
2024-05-06 16:23:16 -0700

    commit for the task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Fabric script to genereate tgz archive
+
+from datetime import datetime
+from fabric.api import *
+
+
+def do_pack():
+    """create a tar archive on web_static folder"""
+
+    datime = datetime.now()
+    archi = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
+    local('mkdir -p versions')
+    create = local('tar -cvzf versions/{} web_static'.format(archi))
+    if create is not None:
+        return archi
+    else:
+        return None
